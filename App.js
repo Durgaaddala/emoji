b2905f691ddcc5deeeaedc00430898c4abd349cd
Durgaaddala <135939453+Durgaaddala@users.noreply.github.com>
2023-07-31 01:56:27 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import EmojiPicker from 'emoji-picker-react'
+function App() {
+  return (
+    <div className="App">
+      <EmojiPicker/>
+    
+    </div>
+  );
+}
+
+export default App;
